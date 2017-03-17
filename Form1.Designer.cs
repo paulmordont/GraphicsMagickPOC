5b3d9925aa1f4cfd1d10956c9bc6e31ef1d29fee
paulmordont <muksalma@tut.by>
2017-03-17 22:13:55 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace GraphicsMagickPOC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.changeBackgroundToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.mainPictureBox = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.addBackgroundImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deconstructToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.imageToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1229, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.addBackgroundImageToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // imageToolStripMenuItem
+            // 
+            this.imageToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.changeBackgroundToolStripMenuItem,
+            this.deconstructToolStripMenuItem});
+            this.imageToolStripMenuItem.Name = "imageToolStripMenuItem";
+            this.imageToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.imageToolStripMenuItem.Text = "Image";
+            // 
+            // changeBackgroundToolStripMenuItem
+            // 
+            this.changeBackgroundToolStripMenuItem.Name = "changeBackgroundToolStripMenuItem";
+            this.changeBackgroundToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.changeBackgroundToolStripMenuItem.Text = "Change background";
+            this.changeBackgroundToolStripMenuItem.Click += new System.EventHandler(this.changeBackgroundToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.MinimumSize = new System.Drawing.Size(200, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 539);
+            this.panel1.TabIndex = 2;
+            // 
+            // mainPictureBox
+            // 
+            this.mainPictureBox.Location = new System.Drawing.Point(3, 3);
+            this.mainPictureBox.Name = "mainPictureBox";
+            this.mainPictureBox.Size = new System.Drawing.Size(331, 518);
+            this.mainPictureBox.TabIndex = 3;
+            this.mainPictureBox.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(664, 518);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(200, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.mainPictureBox);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
+            this.splitContainer1.Size = new System.Drawing.Size(1029, 539);
+            this.splitContainer1.SplitterDistance = 343;
+            this.splitContainer1.TabIndex = 5;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(200, 539);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // addBackgroundImageToolStripMenuItem
+            // 
+            this.addBackgroundImageToolStripMenuItem.Name = "addBackgroundImageToolStripMenuItem";
+            this.addBackgroundImageToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
+            this.addBackgroundImageToolStripMenuItem.Text = "Add background image";
+            this.addBackgroundImageToolStripMenuItem.Click += new System.EventHandler(this.addBackgroundImageToolStripMenuItem_Click);
+            // 
+            // deconstructToolStripMenuItem
+            // 
+            this.deconstructToolStripMenuItem.Name = "deconstructToolStripMenuItem";
+            this.deconstructToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.deconstructToolStripMenuItem.Text = "Flatten";
+            this.deconstructToolStripMenuItem.Click += new System.EventHandler(this.deconstructToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1229, 563);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox mainPictureBox;
+        private System.Windows.Forms.ToolStripMenuItem imageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem changeBackgroundToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem addBackgroundImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deconstructToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphicsMagickPOC
+{
+    using System.Drawing.Imaging;
+    using System.IO;
+    using System.Runtime.Remoting.Messaging;
+
+    using ImageMagick;
+    
+
+    public partial class Form1 : Form
+    {
+        private ImageMagick.MagickImage magickImage;
+
+        private MagickImage layer1;
+
+        private MagickImage layer2;
+
+        //private ImageMagick.MagickImage imageMagickImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.mainPictureBox.Dock = DockStyle.Fill;
+            this.mainPictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            this.pictureBox1.Dock = DockStyle.Fill;
+            this.pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            this.imageToolStripMenuItem.Visible = false;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new OpenFileDialog())
+            {
+                dialog.Filter = "Images|*.jpg;*.jpeg;*.png;*.tif;*.tiff;";
+                dialog.InitialDirectory = @"C:\Users\Pavel_Hubich\Downloads";
+                dialog.Title = "Please select an image";
+                var result = dialog.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    var fileInfo = new FileInfo(dialog.FileName);
+                    if (fileInfo.Exists)
+                    {
+
+                        this.magickImage = new ImageMagick.MagickImage(
+                                               new FileInfo(fileInfo.FullName),
+                                               new MagickReadSettings { Debug = true, Verbose = true });
+                        this.layer1 = new ImageMagick.MagickImage(
+                                               new FileInfo(fileInfo.FullName),
+                                               new MagickReadSettings { Debug = true, Verbose = true, FrameIndex = 1 });
+                        this.layer2 = new ImageMagick.MagickImage(
+                                               new FileInfo(fileInfo.FullName),
+                                               new MagickReadSettings { Debug = true, Verbose = true, FrameIndex = 2 });
+                        var img = new MagickImage();
+                        this.magickImage.Settings.Verbose = true;
+                        this.richTextBox1.Clear();
+                        var sb = new StringBuilder();
+                        foreach (var attr in this.magickImage.AttributeNames)
+                        {
+                            sb.AppendLine(string.Format("{0} : {1}", attr, this.magickImage.GetAttribute(attr)));
+                        }
+                        this.richTextBox1.Text = sb.ToString();
+                        this.mainPictureBox.Image = this.magickImage.ToBitmap();
+                        this.imageToolStripMenuItem.Visible = true;
+                        //var ololo = 
+                    }
+                }
+            }
+        }
+
+        private void changeBackgroundToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (var colorDialog = new ColorDialog())
+            {
+                colorDialog.FullOpen = true;
+                colorDialog.AnyColor = true;
+                var result = colorDialog.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    var col = new MagickImageCollection();
+                    var nImg = new MagickImage(
+                                   new MagickColor(colorDialog.Color),
+                                   this.magickImage.BaseWidth,
+                                   this.magickImage.BaseHeight);
+                    nImg.Compose = CompositeOperator.SrcIn;
+                    var layer = new MagickImage(this.magickImage.ToByteArray(), new MagickReadSettings {FrameIndex = 1 });
+                    col.Add(nImg);
+                    col.Add(layer);
+                    col.Add(this.magickImage);
+                    this.pictureBox1.Image = col.Flatten().ToBitmap();
+                }
+            }
+            
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void addBackgroundImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new OpenFileDialog())
+            {
+                dialog.Filter = "Images|*.jpg;*.jpeg;*.png;*.tif;*.tiff;";
+                dialog.InitialDirectory = @"C:\Users\Pavel_Hubich\Downloads";
+                dialog.Title = "Please select an image";
+                var result = dialog.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    var fileInfo = new FileInfo(dialog.FileName);
+                    if (fileInfo.Exists)
+                    {
+                        var newImage = new ImageMagick.MagickImage(new FileInfo(fileInfo.FullName));
+                        var col = new MagickImageCollection();
+                        //{
+                            newImage.Compose = CompositeOperator.Over;
+                            col.Add(newImage);
+                            col.Add(this.magickImage);
+                            this.pictureBox1.Image = col.Flatten().ToBitmap();
+                        //}
+                    }
+                }
+            }
+        }
+
+        private void deconstructToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            //var newImage = new ImageMagick.MagickImage(new FileInfo(fileInfo.FullName));
+            var col = new MagickImageCollection();
+            //{
+            //this.layer1.Compose = CompositeOperator.Over;
+            //this.layer2.Compose = CompositeOperator.SrcIn;
+            col.Add(this.layer1);
+            col.Add(this.layer2);
+            //col.Combine();
+            //col.Flatten();
+            var flattened = col.Merge();
+            this.pictureBox1.Image = flattened.ToBitmap();
+            //}
+
+        }
+    }
+}
